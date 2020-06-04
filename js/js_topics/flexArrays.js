a69f64f98fccad7038d36c37e1b1baff61f37e0a
akey <alexander.khaniha.ext@siemens.com>
2020-06-04 18:40:17 +0200

    changes 4.6

--- js/js_topics/flexArrays.js
+++ js/js_topics/flexArrays.js
@@ -30,3 +30,51 @@ todos.pop();
 
 console.log(todos);
 
+
+
+
+const breakfastMenuIdeas = ["Buckwheat Pancakes"];
+const dinnerMenuIdeas = ["Glazed Salmon", "Meatloaf", "American Cheeseburger"];
+
+const allMenuIdeas = ["Harvest Salad", "Southern Fried Chicken"];
+
+
+
+const ind = allMenuIdeas.findIndex(idea => idea === 'Harvest Salad' );
+
+
+
+
+const saladIndex = allMenuIdeas.findIndex(idea => idea === 'Harvest Salad');
+
+const meatloafIndex = allMenuIdeas.findIndex(idea => idea === 'Meatloaf');
+
+const finalMenuIdeas = [
+  ...allMenuIdeas.slice(0, meatloafIndex),
+  ...allMenuIdeas.slice(meatloafIndex + 1)
+]
+// const finalMenuIdeas = [
+//   ...allMenuIdeas.slice(0, saladIndex),
+//   "Garden Salad",
+//   ...allMenuIdeas.slice(saladIndex + 1)
+// ];
+
+console.log(ind+"++++++");
+console.log(finalMenuIdeas+" <<<<<");
+
+const finalMenuItems = [
+    "American Cheeseburger",
+    "Southern Fried Chicken",
+    "Glazed Salmon"
+  ];
+  
+  const [winner, ...losers] = finalMenuItems;
+  
+  console.log({ winner, losers });
+  // let [first, second] = finalMenuItems;
+  // [second, first] = [first, second];
+  // let first = finalMenuItems[0];
+  // let second = finalMenuItems[1];
+  // let third = finalMenuItems[2];
+  
+  // console.log(first, second, third);
